21b17392638d0982f7e80c1bf529753569914462
DaiglerT <daiglertj@gmail.com>
2024-06-11 19:17:07 -0400

    got word count to work

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
+
+
+words = file_contents.split()
+count = len(words)
+print(count)
+letters = []
+count = {}
+
+for word in words:
+    word = word.lower()
+    for let in word:
+        letters.append(let)
+for let in letters:
+    if let in count:
+        count[let] += 1
+    else:
+        count[let] = 1
+print(count)
