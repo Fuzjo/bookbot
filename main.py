49b77fe510d81740ec5b2d0f623de3615f66bdad
DaiglerT <daiglertj@gmail.com>
2024-06-11 19:30:20 -0400

    Organized

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
+def word_count(file_contents):
+    words = file_contents.split()
+    count_word = len(words)
+    return count_word, words
+
+def let_count(words):
+    letters = []
+    count = {}
+    for word in words:
+        word = word.lower()
+        for let in word:
+            letters.append(let)
+    for let in letters:
+        if let in count:
+            count[let] += 1
+        else:
+            count[let] = 1
+    return count
+
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
-
-
-words = file_contents.split()
-count = len(words)
-print(count)
-letters = []
-count = {}
-
-for word in words:
-    word = word.lower()
-    for let in word:
-        letters.append(let)
-for let in letters:
-    if let in count:
-        count[let] += 1
-    else:
-        count[let] = 1
-print(count)
+count_words, words = word_count(file_contents)
+count_lets = let_count(words)
+print(count_words, count_lets)
