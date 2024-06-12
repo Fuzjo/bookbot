bcd69b5bf49e3198484f05f80d9831a5ee7ae633
DaiglerT <daiglertj@gmail.com>
2024-06-11 21:32:56 -0400

    Finish

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+path = "books/frankenstein.txt"
+
 def word_count(file_contents):
     words = file_contents.split()
     count_word = len(words)
@@ -16,21 +18,28 @@ def let_count(words):
         else:
             count[let] = 1
     count_list = list(count.items())
-    count_sort = count_list.sort()
-    print(count_sort)
+    count_sort = sorted(count_list, key=lambda letter: letter[1], reverse=True)
     return count, count_sort
 
-with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
+def report(count_words,count_sort):
+    print(f"--- Begin report of books/frankenstein.txt ---")
+    print(f"{count_words} words found in this document")
+    for let in count_sort:
+        letter = let[0]
+        num = let[1]
+        check = letter.isalpha()
+        if check == True:
+            print(f"The {letter} character was found {num} times")
+        else:
+            pass
+    print("--- End Report ---")
+
+def get_book(path):
+    with open(path) as f:
+        file_contents = f.read()
+    return file_contents
+
+file_contents = get_book(path)
 count_words, words = word_count(file_contents)
 count_lets, count_sort = let_count(words)
-
-print(f"--- Begin report of books/frankenstein.txt ---")
-print(f"{count_words} words found in this document")
-for let in count_lets:
-    letter = let[0]
-    check = letter.isalpha()
-    if check == True:
-        print(f"The {letter} character was found {count_lets[f'{let}']} times")
-    else:
-        pass
+report(count_words, count_sort)
